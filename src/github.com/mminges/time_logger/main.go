b82109934ac534ebb3a6f6860a56bd4690922c84
Michael Minges <mminges@ecsteam.com>
2017-11-20 09:23:59 -0700

    adding working template for a dev release of time_logger

--- src/github.com/mminges/time_logger/main.go
+++ src/github.com/mminges/time_logger/main.go
@@ -3,11 +3,14 @@ package main
 import (
     "fmt"
     "time"
+    "flag"
 )
 
 func main() {
+    var period = flag.Int("p",30,"")
+    flag.Parse()
     for {
         fmt.Println("the time is", time.Now())
-        time.Sleep(30 * time.Second)
+        time.Sleep(time.Duration(*period) * time.Second)
     }
 }
